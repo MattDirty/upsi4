60159d427f81a8a7169fe8dc72d333d0381d24be
lfatton <lfatton@LAPTOP-5LDR2FI6>
2024-03-23 02:55:07 +0100

    added bed and floor

--- Utils/Orientation.gd
+++ Utils/Orientation.gd
@@ -8,6 +8,6 @@ const DIRECTIONS_SUBDIVION = RAD_360 / 4 # DIRECTIONS.length
 ## Finds the direction ("East"|"NorthWest"|..) from an angle in radiants
 static func get_direction_from_angle(rad_angle: float):
 	rad_angle += DIRECTIONS_SUBDIVION / 2
-	if rad_angle < 0:
+	if rad_angle <= 0:
 		rad_angle += RAD_360
 	return DIRECTIONS[rad_angle / DIRECTIONS_SUBDIVION]
