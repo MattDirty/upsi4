77ead47f9cd511543ca1c6833b14c7a647b4baf6
m.conti <conti.matthieu@gmail.com>
2024-03-23 17:25:23 +0100

    bullet

---
+++ Entities/Bullet.gd
@@ -0,0 +1,30 @@
+extends AnimatedSprite2D
+
+@export var speed := 120.0
+var velocity: Vector2
+var direction: Vector2
+var layer: int
+
+@export var bulletDuration := 4.0
+
+@onready var lifeTimer := Timer.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocity = direction.normalized() * speed
+	lifeTimer.wait_time = bulletDuration
+	lifeTimer.autostart = true
+	lifeTimer.one_shot = true
+	lifeTimer.timeout.connect(queue_free)
+	add_child(lifeTimer)
+	%Area2D.set_collision_mask_value(layer, true)
+
+
+
+func _physics_process(delta_time):
+	position += velocity * delta_time
+
+
+func _on_area_2d_body_entered(body):
+	body.take_damages(10)
+	queue_free()
