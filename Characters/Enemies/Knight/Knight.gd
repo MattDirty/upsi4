dfeb4dff963d8766541f2610d9db4f48e04e6e8e
lfatton <lfatton@LAPTOP-5LDR2FI6>
2024-03-23 17:19:30 +0100

    enemies with one eye logic

--- Characters/Enemies/Knight/Knight.gd
+++ Characters/Enemies/Knight/Knight.gd
@@ -2,26 +2,36 @@ extends Enemy
 
 var delta_since_last_move: float = 0
 
+
 func idle_roam():
 	var random_rotation = randf() * 2.0 * PI
 	var direction = Vector2(randf_range(15, 45), 0).rotated(random_rotation)
 	velocity = direction * speed
 
 
+func follow_player():
+	var player =  get_tree().root.get_node("Playground/Player")
+	
+	if (player):
+		var direction = (player.position - self.position).normalized()
+		
+		if (direction.x > 0):
+			get_node("CharacterAnimator").scale.x = 1
+		else:
+			get_node("CharacterAnimator").scale.x = -1
+			
+		velocity = direction * speed	
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	delta_since_last_move += delta
-	if delta_since_last_move >= 2:
-		idle_roam()
-		delta_since_last_move = 0
+	follow_player()
 	move_and_slide()
-	super(delta)
 
 
 func _on_hurtbox_body_entered(body):
