d993dc0c9d2f12fa3c7c3683f5d6a861b17b0a47
m.conti <conti.matthieu@gmail.com>
2024-03-23 02:20:49 +0100

    interaction sleep

--- Animations/Hands.gd
+++ Animations/Hands.gd
@@ -2,7 +2,13 @@ extends Node2D
 
 @export var distance: float = 4.0
 
+var action = "Idle"
+
 func _physics_process(_delta_time):
+	if action == "Interact":
+		rotation = 0
+		position = Vector2.ZERO
+		return
 	position = get_target_normalized() * distance
 	look_at(get_parent().global_position)
 	rotate(PI / 2)
@@ -10,3 +16,6 @@ func _physics_process(_delta_time):
 func get_target_normalized() -> Vector2:
 	return get_parent().global_position.direction_to(get_global_mouse_position()).normalized()
 
+func setAnimation(action):
+	print(action)
+	self.action = action
